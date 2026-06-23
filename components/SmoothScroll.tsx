42ea6f3b1c853f250a611b39347e21d3737ac3cd
Harsh Supeda <harsh.supeda1215@gmail.com>
2026-06-24 02:25:22 +0530

    Fix SmoothScroll TypeScript error

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.05, duration: 1.5, smoothWheel: true }}>
-      {children}
+    <ReactLenis root options={{ lerp: 0.05, duration: 1.5 }}>
+      {/* Bypass the strict type check mismatch for Vercel */}
+      {children as any}
     </ReactLenis>
   );
 }
